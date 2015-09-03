4d3c811b9d477a97abd3c85f84494e122fbdbd80
Nadav Blum <nadav.blum@gmail.com>
2015-09-03 22:40:51 +0300

    Finishing up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def score_for_quiz(quiz)
     attempts = @user_previous_attempts.reject {|a| a.quiz_id != quiz.id}
-    attempts.any? ? attempts.max{|a,b| a.score <=> b.score}.score : ""
+    # attempts.any? ? attempts.max{|a,b| a.score <=> b.score}.score : "--"
+    if attempts.any? 
+      attempts.max{|a,b| a.score <=> b.score}.score.to_s + " / " + quiz.num_questions.to_s 
+    else
+      "--"
+    end
   end
 end
